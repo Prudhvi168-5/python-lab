308627a904f52e7e7ccc27a7a580ea92a9068a39
Prudhvi168-5 <prudhvinaidu168@gmail.com>
2026-07-27 19:27:09 +0530

    Add output example to Script_Name_and_Argument_Count.py
    Added example output for clarity.

--- python Assignments/Command Line Arguments/Script_Name_and_Argument_Count.py
+++ python Assignments/Command Line Arguments/Script_Name_and_Argument_Count.py
@@ -3,3 +3,8 @@
 import sys
 print("Script Name:", sys.argv[0])
 print("Total Arguments:", len(sys.argv))
+
+#output
+# Command: python script.py apple banana
+# Script Name: script.py
+# Total Arguments: 3
