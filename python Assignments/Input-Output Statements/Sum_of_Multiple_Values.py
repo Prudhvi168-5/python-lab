8864b51e3a6ae7b6b08a0a5c7c570a559df0c4f9
Prudhvi168-5 <prudhvinaidu168@gmail.com>
2026-07-27 19:15:31 +0530

    Add output example to Sum_of_Multiple_Values.py
    Added output example as comments for clarity.

--- python Assignments/Input-Output Statements/Sum_of_Multiple_Values.py
+++ python Assignments/Input-Output Statements/Sum_of_Multiple_Values.py
@@ -2,3 +2,8 @@
 # Program 4: Sum of Multiple Values
 numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
 print("Sum =", sum(numbers))
+
+
+#output
+# Enter numbers separated by spaces: 10 20 30 40
+# Sum = 100
