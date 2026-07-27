049b8663ac603037bbe9f273a1cc4296f378014c
Prudhvi168-5 <prudhvinaidu168@gmail.com>
2026-07-27 19:14:20 +0530

    Include output examples in Arithmetic_Operations.py
    Added output examples for arithmetic operations.

--- python Assignments/Input-Output Statements/Arithmetic_Operations.py
+++ python Assignments/Input-Output Statements/Arithmetic_Operations.py
@@ -6,3 +6,11 @@ print("Sum =", a + b)
 print("Difference =", a - b)
 print("Product =", a * b)
 print("Quotient =", a / b)
+
+#output
+# Enter first number: 10
+# Enter second number: 5
+# Sum = 15.0
+# Difference = 5.0
+# Product = 50.0
+# Quotient = 2.0
