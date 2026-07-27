046a5103bf2f4a93f953f1bde1e7adb408d072e9
Prudhvi168-5 <prudhvinaidu168@gmail.com>
2026-07-27 19:05:24 +0530

    Add output comments for swapping example
    Added output comments to clarify the swapping results.

--- python Assignments/Variables/Swapping.py
+++ python Assignments/Variables/Swapping.py
@@ -10,3 +10,9 @@ b = 20
 print("Before swapping:", a, b)
 a, b = b, a
 print("After swapping:", a, b)
+
+#output
+#Before swapping: 10 20
+#After swapping: 20 10
+#Before swapping: 10 20
+#After swapping: 20 10
