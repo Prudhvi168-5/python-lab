e4fcd33c10fadd4f0262ca6f757421d1bcbc977b
Prudhvi168-5 <prudhvinaidu168@gmail.com>
2026-07-27 19:13:07 +0530

    Correct import comment and enhance output clarity
    Fixed typo in import comment and added output example.

--- python Assignments/Keywords/identify_keyword.py
+++ python Assignments/Keywords/identify_keyword.py
@@ -1,6 +1,13 @@
+# imporitng keyword
+#Y.Prudhvi Naidu
 import keyword
 key_word=input("enter word:")
 if keyword.iskeyword(key_word):
     print("word is a python keyword")
 else:
     print("word is not a python keyword")
+
+
+#output
+# enter word: apple
+# word is not a python keyword
