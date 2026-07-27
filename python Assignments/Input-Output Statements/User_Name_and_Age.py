1691f42fd97fca762f7810130e62202469c8756a
Prudhvi168-5 <prudhvinaidu168@gmail.com>
2026-07-27 19:18:36 +0530

    Add output example to User_Name_and_Age.py
    Added output example as comments for clarity.

--- python Assignments/Input-Output Statements/User_Name_and_Age.py
+++ python Assignments/Input-Output Statements/User_Name_and_Age.py
@@ -3,3 +3,8 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 print(f"Hello {name}, you will turn {age + 1} next year.")
+
+#output
+# Enter your name: Prudhvi
+# Enter your age: 19
+# Hello Prudhvi, you will turn 20 next year.
